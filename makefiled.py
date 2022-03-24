b0057fa91a3483d66917dd0da799ab5d66ae4e19
haruki1234 <haruki070617@gmail.com>
2022-03-25 07:29:58 +0900

    first commit

---
+++ makefiled.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy
+import copy
+import math
+import os
+from PIL import Image
+
+blocks = [
+    [1000,[185,122,87]],
+    [300 ,[181,230,29]],
+    [400 ,[255,174,201]],
+]
+
+
+def length3d_s(pos) :
+    return (pos[0][0]-pos[1][0])**2+(pos[0][1]-pos[1][1])**2+(pos[0][2]-pos[1][2])**2
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+ifile = "field/field.png"
+ifileb = "field/fieldblock.png"
+ofile = "field/field.js"
+
+image = cv2.imread(ifile)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+imageb = cv2.imread(ifileb)
+imageb = cv2.cvtColor(imageb, cv2.COLOR_BGR2RGB)
+print("original size: "+str(image.shape)) # show original size
+
+size = input("input size: ").split(" ")
+image = cv2.blur(image,(50,50))
+image = cv2.resize(image,(int(size[0]),int(size[1]))) # resize image
+imageb = cv2.resize(imageb,(int(size[0]),int(size[1]))) # resize image
+cv2.imwrite("a.png",image)
+cv2.imwrite("b.png",imageb)
+print("size: "+str(image.shape)) # show resized size
+
+min = image.min()
+max = image.max()
+
+max_elev = 255*0.5*0.3
+
+scale = (max_elev/(max-min))
+
+message = """"""
+rt = ""
+rt += message
+
+f= "\nfield = [\n"
+b= "\nfieldblock = [\n"
+for y in range(image.shape[0]-1):
+    f += "    ["
+    b += "    ["
+    for x in range(image.shape[1]-1):
+        f += '{:5.2f}'.format((max_elev-(image[y,x]*scale-min*scale)+5)) + ','
+
+        px = imageb[y,x]
+        lengt = 255**3
+        block = 0
+        for bl in blocks:
+            l = length3d_s([px.tolist(),bl[1]])
+            # print (([l,px.tolist(),bl[1]]))
+            if l<lengt:
+                lengt = l
+                block = bl[0]
+        b += str(block)+","
+
+    f += "],\n"
+    b += "],\n"
+f += "]"
+b += "]"
+rt += f
+rt += b
+
+#print(rt)
+with open(ofile, mode='w') as f:
+    f.write(rt)
