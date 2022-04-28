e8d6dc9a7b06689963db1aeb0c687cbab42c7b7a
haruki1234 <haruki070617@gmail.com>
2022-04-28 23:44:25 +0900

    make to display ascii-stl

---
+++ fstl.py
@@ -0,0 +1,43 @@
+from scipy.misc import face
+
+
+path = "./whatisthis.stl"
+
+with open(path) as f:
+    data = f.readlines()
+
+
+fctl = []
+lfctl = []
+c = 0
+
+for l in data:
+    if (l.startswith("vertex")):
+        c+=1
+        ld = l.replace("\n","").split(" ")
+        lfctl.append(ld[1])
+        lfctl.append(ld[2])
+        lfctl.append(ld[3])
+        if (c%3==0):
+            fctl.append(lfctl)
+            # print(lfctl)
+            lfctl = []
+
+lt = "cube = "
+lt += "[ [0],"
+lt += "["
+
+for i in range(len(fctl)):
+    lt += '['
+    lt += "["+fctl[i][0]+","+fctl[i][1]+","+fctl[i][2]+"],["+fctl[i][3]+","+fctl[i][4]+","+fctl[i][5]+"],["+fctl[i][6]+","+fctl[i][7]+","+fctl[i][8]+"],"
+    lt += "[100000],[0,0],[0,128],[128,128]"
+    lt += '],'
+
+lt += "]]"
+
+# print(lt)
+
+path_w = './object.txt'
+
+wf = open(path_w, mode='w')
+wf.write(lt)
