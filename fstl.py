10e5672c3961d0a8d918cafd7d802a67d793f4b2
haruki1234 <haruki070617@gmail.com>
2022-04-29 19:46:28 +0900

    saturn

--- fstl.py
+++ fstl.py
@@ -2,42 +2,44 @@ from numpy import double
 from scipy.misc import face
 
 
-path = "./whatisthis.stl"
+paths = [
+    ["./whatisthis.stl",[230,160,50],[0,0,0]],
+]
 
-with open(path) as f:
-    data = f.readlines()
-
-
-fctl = []
-lfctl = []
-c = 0
-
-for l in data:
-    if (l.startswith("vertex")):
-        c+=1
-        ld = l.replace("\n","").split(" ")
-        lfctl.append(str(double(ld[1])))
-        lfctl.append(str(double(ld[2])))
-        lfctl.append(str(double(ld[3])))
-        if (c%3==0):
-            fctl.append(lfctl)
-            # print(lfctl)
-            lfctl = []
 
 lt = "cube = "
 lt += "[ [0],"
 lt += "["
 
-for i in range(len(fctl)):
-    lt += '['
-    lt += "["+fctl[i][0]+","+fctl[i][1]+","+fctl[i][2]+"],["+fctl[i][3]+","+fctl[i][4]+","+fctl[i][5]+"],["+fctl[i][6]+","+fctl[i][7]+","+fctl[i][8]+"],"
-    lt += "[100000],[0,0],[0,128],[128,128],"
-    lt += "[0],"
-    lt += '],'
+for path in paths:
+    with open(path[0]) as f:
+        data = f.readlines()
+
+    color = "[0],["+str(path[1][0])+"],["+str(path[1][1])+"],["+str(path[1][2])+"],"
+
+    fctl = []
+    lfctl = []
+    c = 0
+
+    for l in data:
+        if (l.startswith("vertex")):
+            c+=1
+            ld = l.replace("\n","").split(" ")
+            lfctl.append(str(double(ld[1])+path[2][0]))
+            lfctl.append(str(double(ld[2])+path[2][1]))
+            lfctl.append(str(double(ld[3])+path[2][2]))
+            if (c%3==0):
+                fctl.append(lfctl)
+                # print(lfctl)
+                lfctl = []
+    for i in range(len(fctl)):
+        lt += '['
+        lt += "["+fctl[i][0]+","+fctl[i][1]+","+fctl[i][2]+"],["+fctl[i][3]+","+fctl[i][4]+","+fctl[i][5]+"],["+fctl[i][6]+","+fctl[i][7]+","+fctl[i][8]+"],"
+        lt += color
+        lt += "[0],"
+        lt += '],'
 
 lt += "]]"
-
-# print(lt)
 
 path_w = './object.txt'
 
