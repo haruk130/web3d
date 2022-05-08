ff0f5a2c0b3a1e1c245802be3413254a02d9304e
haruki1234 <haruki070617@gmail.com>
2022-05-09 00:03:36 +0900

    change a little

--- fstl.py
+++ fstl.py
@@ -85,6 +85,44 @@ for path in paths:
 
 lt += "]];\n"
 
+paths = [
+   ["./triangle.stl",[250,255,254],[0,0,0]],
+]
+
+lt += "scene['triangle'] = "
+lt += "[ [0],"
+lt += "["
+
+for path in paths:
+    with open(path[0]) as f:
+        data = f.readlines()
+
+    color = "[0],["+str(path[1][0])+"],["+str(path[1][1])+"],["+str(path[1][2])+"],"
+
+    fctl = []
+    lfctl = []
+    c = 0
+
+    for l in data:
+        if (l.startswith("vertex")):
+            c+=1
+            ld = l.replace("\n","").split(" ")
+            lfctl.append(str(double(ld[1])+path[2][0]))
+            lfctl.append(str(double(ld[2])+path[2][1]))
+            lfctl.append(str(double(ld[3])+path[2][2]))
+            if (c%3==0):
+                fctl.append(lfctl)
+                # print(lfctl)
+                lfctl = []
+    for i in range(len(fctl)):
+        lt += '['
+        lt += "["+fctl[i][0]+","+fctl[i][1]+","+fctl[i][2]+"],["+fctl[i][3]+","+fctl[i][4]+","+fctl[i][5]+"],["+fctl[i][6]+","+fctl[i][7]+","+fctl[i][8]+"],"
+        lt += color
+        lt += "[0],"
+        lt += '],'
+
+lt += "]];\n"
+
 path_w = './object.js'
 
 wf = open(path_w, mode='w')
